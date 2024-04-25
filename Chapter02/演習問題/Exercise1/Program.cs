5d3b0c53f422c38823faa7aff1b30eb0bf30becf
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-04-25 14:24:35 +0900

    演習問題完了

---
+++ Chapter02/演習問題/Exercise1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    internal class Program {
+        static void Main(string[] args) {
+            // 2.1.3
+            var songs = new Song[] {
+                new Song("キセキ","a",153),
+                new Song("マリーゴールド","b",120),
+                new Song("365日","ac",170),
+            };
+            PrintSongs(songs);
+        }
+
+        // 2.1.4
+        private static void PrintSongs(Song[] songs) { 
+
+            foreach (var song in songs) {
+                //ヒント 153秒の場合(00:02:23)
+                Console.WriteLine(@"{0},{1} {2:mm\:ss}",song.Title,song.ArtistName,
+                                    TimeSpan.FromSeconds(song.Length));
+            }
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise1/Song.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    internal class Song {
+        // 2.1.1
+        //歌のタイトル
+        public string Title { get; set; }
+        //アーティスト名
+        public string ArtistName { get; set; }
+        //演奏時間(単位は秒)
+        public int Length { get; set; }
+
+        //2.1.2
+        //コンストラクタ
+        public Song(string Title, string ArtistName, int Length) { 
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length; 
+        }
+
+    }
+}
