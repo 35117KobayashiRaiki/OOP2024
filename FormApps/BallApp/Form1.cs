72cd583a49fe217bdf465edbbfdf3b56dc532e82
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-05-08 16:00:10 +0900

    サッカーボール、テニスボールのリスト完成

--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -1,7 +1,9 @@
 namespace BallApp {
     public partial class Form1 : Form {
-        Obj ball;
-        PictureBox pb;
+        
+        //Listコレクション
+        private List<Obj> balls = new List<Obj>();  //ボールインスタンス格納用
+        private List<PictureBox> pbs = new List<PictureBox>();  //表示用
 
         //コンストラクタ
         public Form1() {
@@ -16,15 +18,16 @@ namespace BallApp {
 
         private void timer1_Tick(object sender, EventArgs e) {
 
-            ball.Move();
-            pb.Location = new Point((int)ball.PosX, (int)ball.PosY);
-
+            for (int i = 0; i < balls.Count; i++) {
+                balls[i].Move();
+                pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);
+            }
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e) {
 
-            pb = new PictureBox();   //画像を表示するコントロール
-            pb.Size = new Size(50, 50);
+            PictureBox pb = new PictureBox();   //画像を表示するコントロール
+            Obj ball = null;
 
             if (e.Button == MouseButtons.Left) {
                 ball = new SoccerBall(e.X - 25, e.Y - 25);
@@ -40,6 +43,9 @@ namespace BallApp {
             pb.SizeMode = PictureBoxSizeMode.StretchImage;
             pb.Parent = this;
             timer1.Start();
+
+            balls.Add(ball);
+            pbs.Add(pb);
         }
     }
 }
