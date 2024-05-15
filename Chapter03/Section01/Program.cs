a9754ceb53a27b1a6894c1534450fd7f68c987d0
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-05-15 15:58:10 +0900

    演習問題3.2準備

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -9,21 +9,28 @@ namespace Section01 {
     internal class Program {
         static void Main(string[] args) {
             var names = new List<string> {
-                "Tokyo", 
-                "New Delhi", 
-                "Bangkok", 
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
                 "London",
-                "Paris", 
-                "Berlin", 
-                "Canberra", 
+                "Paris",
+                "Berlin",
+                "Canberra",
                 "Hong Kong",
              };
 
-            IEnumerable<string> query = names.Where(s => s.Contains(" "))
-                                             .Select(s => s.ToUpper());
-            foreach (string s in query)
-                Console.WriteLine(s);
-                
+            var query = names.Where(s => s.Length <= 5).ToList();
+            foreach (var item in query){
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("-----------");
+
+            names[0] = "Osake";
+            foreach (var item in query){
+                Console.WriteLine(item);
+            }
+
         }
     }
 }
