65457ddfd48f40d927fefdb1cb03148155d3a8d5
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-05-30 13:40:43 +0900

    5.2完成

--- Chapter05/演習問題/Exercise02/Program.cs
+++ Chapter05/演習問題/Exercise02/Program.cs
@@ -8,6 +8,20 @@ namespace Exercise02 {
     internal class Program {
         static void Main(string[] args) {
 
+            string input = Console.ReadLine();
+            int num;
+
+            if (int.TryParse(input, out num)) {
+                var comma = num.ToString("N0");
+                Console.WriteLine(comma);
+            } else {
+                Console.WriteLine("入力された文字列は数値に変換できませんでした。");
+            }
         }
+
+        private static string FormatNumberWithCommas(int number) {
+            return number.ToString("N0");
+        }
+
     }
 }
