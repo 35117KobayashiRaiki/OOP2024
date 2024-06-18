6438204284037476870f1b63a8a355640dc19028
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-06-18 11:32:41 +0900

    設問1_提出

--- TestProject/Test01/ScoreCounter.cs
+++ TestProject/Test01/ScoreCounter.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Test01 {
@@ -11,22 +12,33 @@ namespace Test01 {
         }
 
 
-        //メソッドの概要： 
+        //メソッドの概要： 科目別点数を読み込み、Studentオブジェクトのリストを返す
         private static IEnumerable<Student> ReadScore(string filePath) {
-
-
-
-
-
+            var students = new List<Student>();
+            var lines = File.ReadAllLines(filePath);
+            foreach (var line in lines){
+                Console.WriteLine(line);
+                var items = line.Split(',');
+                var student = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2]),
+                };
+                students.Add(student);
+            }
+            return students;
         }
 
-        //メソッドの概要： 
+        //メソッドの概要： 科目別の点数を求める
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
+            var dict = new Dictionary<string, int>();
+            foreach (var score in _score) {
+                if (dict.ContainsKey(score.Name))
+                    dict[score.Subject] += score.Score;
+                else
+                    dict[score.Subject] = score.Score;
+            }
+            return dict;
         }
     }
 }
