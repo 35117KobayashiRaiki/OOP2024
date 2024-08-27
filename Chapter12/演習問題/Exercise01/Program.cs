cb03434cc1b198d9f6f3a811b36f39607ea9bb0d
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-08-27 11:17:09 +0900

    12.1.1完成

---
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_1("employee.xml");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employee.xml"));
+            Console.WriteLine();
+
+            Exercise1_2("employees.xml");
+            Exercise1_3("employees.xml");
+            Console.WriteLine();
+
+            Exercise1_4("employees.json");
+
+            // これは確認用
+            Console.WriteLine(File.ReadAllText("employees.json"));
+        }
+
+        private static void Exercise1_1(string outfile) {
+            Console.WriteLine(File.ReadAllText("employee.xml"));
+            
+        }
+
+        private static void Exercise1_2(string outfile) {
+            
+        }
+
+        private static void Exercise1_3(string v) {
+            
+        }
+
+        private static void Exercise1_4(string v) {
+            
+        }
+    }
+}
