d8873b09f13feea417b60af012feb4154f0fc4bf
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-09-03 11:23:23 +0900

    RSS ListBox 完成

---
+++ FormApps/RssReader/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbRssUrl.Text);
+                var xdoc = XDocument.Load(url);
+
+                var xtitles = xdoc.Descendants("item");
+                foreach (var item in xtitles) {
+                    var title = item.Element("title")?.Value;
+                    if (!string.IsNullOrEmpty(title)) {
+                        lbRssTitle.Items.Add(title);
+                    }
+
+                }
+            }
+        }
+    }
+}
