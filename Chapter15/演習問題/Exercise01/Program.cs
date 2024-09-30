bc12cd66ca5f3d5f411e8d749467539986db463c
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-09-30 14:29:39 +0900

    Exercise1_2完成

---
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -0,0 +1,62 @@
+﻿using Section01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            var price = Library.Books
+                .Max(b => b.Price);
+
+            var book = Library.Books
+                .First(b => b.Price == price);
+
+            Console.WriteLine(book);
+        }
+
+        private static void Exercise1_3() {
+            
+        }
+
+        private static void Exercise1_4() {
+            
+        }
+
+        private static void Exercise1_5() {
+            
+        }
+
+        private static void Exercise1_6() {
+            
+        }
+
+        private static void Exercise1_7() {
+            
+        }
+
+        private static void Exercise1_8() {
+            
+        }
+    }
+}
