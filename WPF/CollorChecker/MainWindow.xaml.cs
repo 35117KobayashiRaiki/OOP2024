328eceb69d6aa1b115113b6014aee57b52fb865a
KobayashiRaiki <166583783+35117KobayashiRaiki@users.noreply.github.com>
2024-10-21 15:58:44 +0900

    stockButton_Clickまで完成

--- WPF/CollorChecker/MainWindow.xaml.cs
+++ WPF/CollorChecker/MainWindow.xaml.cs
@@ -18,8 +18,36 @@ namespace CollorChecker {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+
+        private List<Color> colorStock = new List<Color>();
+
         public MainWindow() {
             InitializeComponent();
+            UpdateColorValues();
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            UpdateColorValues();
+        }
+
+        private void UpdateColorValues() {
+            int r = (int)rSlider.Value;
+            int g = (int)gSlider.Value;
+            int b = (int)bSlider.Value;
+
+            rValue.Text = r.ToString();
+            gValue.Text = g.ToString();
+            bValue.Text = b.ToString();
+
+            Color color = Color.FromRgb((byte)r, (byte)g, (byte)b);
+            colorArea.Background = new SolidColorBrush(color);
+
+        }
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+            Color currentColor = ((SolidColorBrush)colorArea.Background).Color;
+            colorStock.Add(currentColor);
+            stockList.Items.Add(currentColor.ToString());
         }
     }
 }
